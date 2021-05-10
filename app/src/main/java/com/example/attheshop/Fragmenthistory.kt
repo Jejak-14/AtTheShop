24bfd27b3da70405b1f175fabbc993b84274cd0f
Jejak-14 <jejak14@student.sdu.dk>
2021-05-10 15:18:52 +0200

    Fragment Page

---
+++ app/src/main/java/com/example/attheshop/Fragmenthistory.kt
@@ -0,0 +1,11 @@
+package com.example.attheshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Fragmenthistory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragmenthistory)
+    }
+}
