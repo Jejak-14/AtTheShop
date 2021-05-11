3711483bbec4b8accbba9aaa3e4400dd6b4c001c
Jejak-14 <jejak14@student.sdu.dk>
2021-05-11 12:19:22 +0200

    insert Fragments % manifest

--- app/src/main/java/com/example/attheshop/Fragmenthistory.kt
+++ app/src/main/java/com/example/attheshop/Fragmenthistory.kt
@@ -7,5 +7,8 @@ class Fragmenthistory : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragmenthistory)
+
+
+
     }
 }
