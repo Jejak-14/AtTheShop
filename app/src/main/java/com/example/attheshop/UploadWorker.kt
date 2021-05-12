a9715f26d969171f33ba807e21d0be726494ddaa
Nickolai <nipet18@student.sdu.dk>
2021-05-12 08:40:57 +0100

    Workmanager BETA Test

---
+++ app/src/main/java/com/example/attheshop/UploadWorker.kt
@@ -0,0 +1,20 @@
+package com.example.attheshop
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import java.lang.Exception
+
+class UploadWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
+    override fun doWork(): Result {
+        return try {
+            for (i in 0..600) {
+                Log.i("MYTAG", "Uploading $i")
+            }
+            Result.success()
+        } catch (e: Exception) {
+            Result.failure()
+        }
+    }
+}
