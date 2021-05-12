69cd64ea4b6d2c339565e04680a541adee28d42b
Nickolai <nipet18@student.sdu.dk>
2021-05-12 08:40:57 +0100

    Workmanager BETA Test

--- app/src/main/java/com/example/attheshop/PlanningActivity.kt
+++ app/src/main/java/com/example/attheshop/PlanningActivity.kt
@@ -1,11 +1,41 @@
 package com.example.attheshop
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.lifecycle.Observer
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
+
 
 class PlanningActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_planning)
+
+        val btnSTART = findViewById<Button>(R.id.btnAccept)
+
+
+        // set on-click listener
+        btnSTART.setOnClickListener {
+            setOneTimeWorkRequest()
+        }
+    }
+
+    private var textView = findViewById<View>(R.id.theProposition)
+    private fun setOneTimeWorkRequest() {
+        val workManager: WorkManager = WorkManager.getInstance(applicationContext)
+        val uploadRequest: OneTimeWorkRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            .build()
+        workManager.enqueue(uploadRequest)
+        workManager.getWorkInfoByIdLiveData(uploadRequest.id)
+            .observe(this, Observer {
+
+        })
     }
 }
+
